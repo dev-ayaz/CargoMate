0a458c6e427434c044a7eae473656c99a132b03a
dev_ayaz <dev_ayaz@yahoo.com>
2017-06-29 02:40:04 +0500

    Customer

---
+++ CargoMate.Web/Models/Customer/CustomerViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CargoMateSolution.Models.Customer
+{
+    public class CustomerViewModel
+    {
+        public long Id { get; set; }
+
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string CustomerId { get; set; }
+
+        [StringLength(50)]
+        public string EmailAddress { get; set; }
+
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public bool? Gender { get; set; }
+    }
+}
